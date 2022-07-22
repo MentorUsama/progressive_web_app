b1851dc6b15054d79c186cb5c619a2a2522dc2d3
unknown <muhammadusamafarhat100@gmail.com>
2022-07-22 20:57:58 +0500

    dynamic cache added

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", (event) => {
     caches.open("static").then((cache) => {
       console.log("[service worker] pre caching");
       cache.addAll([
-        '/',
+        "/",
         "./index.html",
         "./src/js/app.js",
         "./src/js/feed.js",
@@ -16,8 +16,8 @@ self.addEventListener("install", (event) => {
         "./src/images/main-image.jpg",
         "https://fonts.googleapis.com/css?family=Roboto:400,700",
         "https://fonts.googleapis.com/icon?family=Material+Icons",
-        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
-        ]);
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
     })
   );
 });
@@ -32,7 +32,15 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) return response;
-      else return fetch(event.request);
+      else
+        return fetch(event.request).then((res) => {
+          return caches.open("dynamic").then((res) => {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        }).catch(error=>{
+
+        });
     })
   ); // Overwrite data that is get back
 });
