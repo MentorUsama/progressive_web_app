a19ba7f38383238a43133d3728482db1b0268ca4
unknown <muhammadusamafarhat100@gmail.com>
2022-07-22 18:34:38 +0500

    multi page caching added

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,21 @@ self.addEventListener("install", (event) => {
   event.waitUntill(
     caches.open("static").then((cache) => {
       console.log("[service worker] pre caching");
-      cache.add("./src/js/app.js");
+      cache.addAll([
+        '/',
+        "./index.html",
+        "./src/js/app.js",
+        "./src/js/feed.js",
+        "./src/js/promise.js",
+        "./src/js/fetch.js",
+        "./src/js/material.min.js",
+        "./src/css/app.css",
+        "./src/css/feed.css",
+        "./src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+        ]);
     })
   );
 });
@@ -15,7 +29,6 @@ self.addEventListener("activate", (event) => {
 
 // Trigger when something is fetched or we manually send fetched event
 self.addEventListener("fetch", (event) => {
-  console.log("[Service worker] fetching....", event);
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) return response;
