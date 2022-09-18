f339daf19ec58910b16fdd4ab5ce5a5db0c9286e
MentorUsama <usama.farhat.99@gmail.com>
2022-09-18 12:53:25 +0500

    notification added

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,48 +1,66 @@
-deferredPrompt=null
-var enableNotificationsButtons=document.querySelectorAll('.enable-notifications')
+deferredPrompt = null;
+var enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
 
 if (!window.Promise) {
-    window.Promise = Promise;
+  window.Promise = Promise;
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("../../sw.js").then(() => {
+    console.log("Service Worker Is Register");
+  });
+}
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log("beforeinstallprompt is fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+function displayConfirmNotification() {
+  var options = {
+    body: "You successfully subscribed to our Notification services ",
+    icon:'/src/images/icons/app-icon-96x96.png',
+    image:'/src/images/sf-boat.jpg',
+    dir:'ltr',
+    lang:'en-US', // BCP 47,
+    vibrate:[100,50,200],
+    badge:'/src/images/icons/app-icon-96x96.png', // In android
+    tag:'confirm-notification', // For same notification they will replace each other if their is multiple one
+    renotify:true // New notification of same tag will vibrate
+  };
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then(function (swreg) {
+      swreg.showNotification(
+        "Successfully subscribed (FROM SW!!)",
+        options
+      );
+    });
+  } else {
+    new Notification("Successfully subscribed", options);
   }
-  
-if('serviceWorker' in navigator)
-{
-    navigator.serviceWorker.register('../../sw.js')
-    .then(()=>{
-        console.log("Service Worker Is Register")
-    })
 }
-
-window.addEventListener("beforeinstallprompt",(event)=>{
-    console.log("beforeinstallprompt is fired")
-    event.preventDefault()
-    deferredPrompt=event
-    return false
-})
-
-function displayConfirmNotification(){
-    var options = {
-        body:'You successfully subscribed to our Notification services '
+function askForNotificationPermission() {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result != "granted") {
+      console.log("Permission Is Not Granted");
+    } else {
+      displayConfirmNotification();
     }
-    new Notification("Successfully subscribed",options)
+  });
 }
-function askForNotificationPermission(){
-    Notification.requestPermission(function(result){
-        console.log("User Choice",result)
-        if(result!='granted'){
-            console.log("Permission Is Not Granted")
-        }
-        else{
-            displayConfirmNotification()
-        }
-    })
-}
-if("Notification" in window){
-    for(var i=0;i<enableNotificationsButtons.length;i++){
-        enableNotificationsButtons[i].style.display='inline-block'
-        enableNotificationsButtons[i].addEventListener('click',askForNotificationPermission)
-    }
-}
-else{
-    console.log("Notification Not Found")
+if ("Notification" in window) {
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = "inline-block";
+    enableNotificationsButtons[i].addEventListener(
+      "click",
+      askForNotificationPermission
+    );
+  }
+} else {
+  console.log("Notification Not Found");
 }
