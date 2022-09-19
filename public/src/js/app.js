52a4e57bbdf44eada001a3492c0ca408314ee2c4
MentorUsama <usama.farhat.99@gmail.com>
2022-09-19 21:40:25 +0500

    web push added

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -20,37 +20,59 @@ window.addEventListener("beforeinstallprompt", (event) => {
   return false;
 });
 
-function displayConfirmNotification() {
-  var options = {
-    body: "You successfully subscribed to our Notification services ",
-    icon: "/src/images/icons/app-icon-96x96.png",
-    image: "/src/images/sf-boat.jpg",
-    dir: "ltr",
-    lang: "en-US", // BCP 47,
-    vibrate: [100, 50, 200],
-    badge: "/src/images/icons/app-icon-96x96.png", // In android
-    tag: "confirm-notification", // For same notification they will replace each other if their is multiple one
-    renotify: true, // New notification of same tag will vibrate,
-    actions: [
-      {
-        action: "confirm",
-        title: "Ok",
-        icon: "/src/images/icons/app-icon-96x96.png",
-      },
-      {
-        action: "cancel",
-        title: "Cancel",
-        icon: "/src/images/icons/app-icon-96x96.png",
-      },
-    ],
-  };
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready.then(function (swreg) {
-      swreg.showNotification("Successfully subscribed (FROM SW!!)", options);
-    });
-  } else {
-    new Notification("Successfully subscribed", options);
+// ============ Displaying Notification by javascript ============
+// function displayConfirmNotification() {
+//   var options = {
+//     body: "You successfully subscribed to our Notification services ",
+//     icon: "/src/images/icons/app-icon-96x96.png",
+//     image: "/src/images/sf-boat.jpg",
+//     dir: "ltr",
+//     lang: "en-US", // BCP 47,
+//     vibrate: [100, 50, 200],
+//     badge: "/src/images/icons/app-icon-96x96.png", // In android
+//     tag: "confirm-notification", // For same notification they will replace each other if their is multiple one
+//     renotify: true, // New notification of same tag will vibrate,
+//     actions: [
+//       {
+//         action: "confirm",
+//         title: "Ok",
+//         icon: "/src/images/icons/app-icon-96x96.png",
+//       },
+//       {
+//         action: "cancel",
+//         title: "Cancel",
+//         icon: "/src/images/icons/app-icon-96x96.png",
+//       },
+//     ],
+//   };
+//   if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.ready.then(function (swreg) {
+//       swreg.showNotification("Successfully subscribed (FROM SW!!)", options);
+//     });
+//   } else {
+//     new Notification("Successfully subscribed", options);
+//   }
+// }
+function configurePushSub() {
+  if (!("serviceWorker" in navigator)) {
+    return;
   }
+  var reg;
+  navigator.serviceWorker.ready
+    .then(function (swreg) {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function (sub) {
+      if (sub == null) {
+        // Create a new subscription
+        reg.pushManager.subscribe({
+          userVisibleOnly:true
+        });
+      } else {
+        // We have a subscription
+      }
+    });
 }
 function askForNotificationPermission() {
   Notification.requestPermission(function (result) {
@@ -58,11 +80,12 @@ function askForNotificationPermission() {
     if (result != "granted") {
       console.log("Permission Is Not Granted");
     } else {
-      displayConfirmNotification();
+      configurePushSub();
+      // displayConfirmNotification();
     }
   });
 }
-if ("Notification" in window) {
+if ("Notification" in window && "serviceWorker" in navigator) {
   for (var i = 0; i < enableNotificationsButtons.length; i++) {
     enableNotificationsButtons[i].style.display = "inline-block";
     enableNotificationsButtons[i].addEventListener(
--- public/sw.js
+++ public/sw.js
@@ -206,18 +206,21 @@ self.addEventListener("sync", function (event) {
   }
 });
 
-self.addEventListener("notificationclick", function (event) {
-  var notification = event.notification;
-  var action = event.action;
-  console.log(notification);
-  if (action == "confirm") {
-    console.log("Confirm Was Choosen");
-    notification.close();
-  } else {
-    console.log("Another action was choosen",action)
-  }
-});
 
-self.addEventListener('notificationclose', function(event) {
-  console.log('Notification was closed', event);
-});
+
+// ============ Service Worker Listners ============
+// self.addEventListener("notificationclick", function (event) {
+//   var notification = event.notification;
+//   var action = event.action;
+//   console.log(notification);
+//   if (action == "confirm") {
+//     console.log("Confirm Was Choosen");
+//     notification.close();
+//   } else {
+//     console.log("Another action was choosen",action)
+//   }
+// });
+
+// self.addEventListener('notificationclose', function(event) {
+//   console.log('Notification was closed', event);
+// });
