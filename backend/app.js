f32f7873ccc50dfd1ae1f6c61ea95b5608cecca8
MentorUsama <usama.farhat.99@gmail.com>
2022-09-24 22:30:29 +0500

    code formate change

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,34 @@ const bodyParser = require("body-parser");
 const webpush = require("web-push");
 var admin = require("firebase-admin");
 const path = require("path");
+const multer = require("multer");
+
+// ====== Environment Variable ======
 require("dotenv").config();
+
+// ====== Multer Initilizing =====
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "file");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// ===== Initilizing Firebase =====
 var serviceAccount = require(path.join(__dirname, "service_account.json"));
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -11,9 +38,8 @@ admin.initializeApp({
 });
 var ad = admin.database();
 
+// ====== Initilizing Heders For APIS ======
 const app = express();
-app.use(bodyParser.json()); // application/json
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -24,7 +50,20 @@ app.use((req, res, next) => {
   next();
 });
 
+// ====== Initilizing global middleware ======
+app.use(
+  multer({
+    dest: "file",
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single("file")
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ====== Post Routes ======
 app.post("/post", async (request, response, next) => {
+  console.log(request.body);
   // =============== Storung Data into database ===============
   try {
     await admin.database().ref("posts").push({
@@ -42,11 +81,9 @@ app.post("/post", async (request, response, next) => {
     "BBo-u3rf39PHr7FZM4P5Mm795QXaGsTnTL5DnCn9Drij5DBZctiMeMmniez9ldSQehR6hcK_zYOrtr1WSmELEZY",
     process.env.PRIVATE_KEY
   );
+  var subcriptions;
   try {
-    const subcriptions = await admin
-      .database()
-      .ref("subscriptions")
-      .once("value");
+    subcriptions = await admin.database().ref("subscriptions").once("value");
   } catch (error) {
     return response.status(500).json({ error: error });
   }
